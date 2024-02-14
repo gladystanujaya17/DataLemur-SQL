062939dfced7aee8782efbbb47e6d851accc3920
Gladys Tanujaya <120657612+gladystanujaya17@users.noreply.github.com>
2024-02-14 12:15:35 +0700

    Delete 03_Average_Review_Ratings.sql

--- 03_Average_Review_Ratings.sql
+++
@@ -1,8 +0,0 @@
---Average Review Ratings [Amazon SQL Interview Question]
-SELECT 
-  EXTRACT(MONTH FROM submit_date) AS mth, 
-  product_id AS product, 
-  ROUND(AVG(stars), 2) AS avg_stars
-FROM reviews
-GROUP BY product_id, EXTRACT(MONTH FROM submit_date)
-ORDER BY mth, product_id ASC;
