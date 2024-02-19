de09b424737ed3d63e397d6797e9eec63b86911e
Gladys Tanujaya <120657612+gladystanujaya17@users.noreply.github.com>
2024-02-19 22:09:38 +0700

    Add files via upload

---
+++ Easy/06_Teams_Power_Users.sql
@@ -0,0 +1,9 @@
+--Teams Power Users [Microsoft SQL Interview Question]
+SELECT sender_id, COUNT(message_id) AS message_count
+FROM messages
+WHERE 
+  EXTRACT(MONTH FROM sent_date) = '8' 
+  AND EXTRACT(YEAR FROM sent_date) = '2022'
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2;
