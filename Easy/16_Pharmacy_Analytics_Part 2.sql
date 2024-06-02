47dbff9c2b28f7f6a5126b872fdb19171747efb3
Gladys Tanujaya <120657612+gladystanujaya17@users.noreply.github.com>
2024-06-02 20:22:57 +0700

    Add files via upload

---
+++ Easy/16_Pharmacy_Analytics_Part 2.sql
@@ -0,0 +1,9 @@
+--Pharmacy Analytics (Part 2) [CVS Health SQL Interview Question]
+SELECT 
+  manufacturer, 
+  COUNT(drug) AS drug_count,
+  ABS(SUM(total_sales - cogs)) AS net_value
+FROM pharmacy_sales
+WHERE (total_sales - cogs) <= 0
+GROUP BY manufacturer
+ORDER BY drug_count DESC, manufacturer ASC;
